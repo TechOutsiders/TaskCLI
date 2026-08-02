ad0168458eea91251528954a6144b31c6110cee1
IvanDetkov <ivandetkov.job@gmail.com>
2026-08-02 17:44:45 +0300

    review changes

--- internal/model/task.go
+++ internal/model/task.go
@@ -1,11 +1,17 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 // Task holds all the information about a single to-do item.
 type Task struct {
-	ID          int
+	ID          uuid.UUID
 	Title       string
 	Description string
 	Status      Status
 	Priority    Priority
-	Created     string
+	CreatedAt   time.Time
 }
