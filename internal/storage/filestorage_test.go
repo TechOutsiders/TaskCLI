fa66a781b3f74191b7a32e2d4e31a3782cce9b3c
IvanDetkov <ivandetkov.job@gmail.com>
2026-08-17 02:39:35 +0300

    improve code and add some test cases

--- internal/storage/filestorage_test.go
+++ internal/storage/filestorage_test.go
@@ -11,40 +11,83 @@ import (
 )
 
 func TestFileStorageSave(t *testing.T) {
-	t.Run("saves one task", func(t *testing.T) {
-		dir := t.TempDir()
-		path := filepath.Join(dir, "tasks.json")
+	dir := t.TempDir()
+	path := filepath.Join(dir, "tasks.json")
 
-		storage, err := NewFileStorage(path)
-		if err != nil {
-			t.Fatalf("NewfileStorage() error: %v", err)
-		}
+	storage, err := NewFileStorage(path)
+	if err != nil {
+		t.Fatalf("NewfileStorage() error: %v", err)
+	}
 
-		createdAt := time.Now().UTC().Round(0)
+	createdAt := time.Now().UTC().Round(0)
 
-		expected := []model.Task{
-			{
-				ID:          uuid.New(),
-				Title:       "Testing",
-				Description: "Learning how to write tests",
-				Status:      model.StatusToDo,
-				Priority:    model.PriorityHigh,
-				CreatedAt:   createdAt,
+	testCases := []struct {
+		name  string
+		tasks []model.Task
+	}{
+		{
+			name: "one task",
+			tasks: []model.Task{
+				{
+					ID:          uuid.New(),
+					Title:       "Testing",
+					Description: "Learning how to write tests",
+					Status:      model.StatusToDo,
+					Priority:    model.PriorityHigh,
+					CreatedAt:   createdAt,
+				},
 			},
-		}
+		},
+		{
+			name: "multiple task",
+			tasks: []model.Task{
+				{
+					ID:          uuid.New(),
+					Title:       "Testing",
+					Description: "Learning how to write tests",
+					Status:      model.StatusToDo,
+					Priority:    model.PriorityHigh,
+					CreatedAt:   createdAt,
+				},
+				{
+					ID:          uuid.New(),
+					Title:       "Testing number 2",
+					Description: "Learning how to write tests",
+					Status:      model.StatusInProgress,
+					Priority:    model.PriorityMedium,
+					CreatedAt:   createdAt.Add(time.Hour),
+				},
+				{
+					ID:          uuid.New(),
+					Title:       "Testing number 2",
+					Description: "Learning how to write tests",
+					Status:      model.StatusInProgress,
+					Priority:    model.PriorityMedium,
+					CreatedAt:   createdAt.Add(2 * time.Hour),
+				},
+			},
+		},
+		{
+			name:  "empty task list",
+			tasks: []model.Task{},
+		},
+	}
 
-		err = storage.Save(expected)
-		if err != nil {
-			t.Fatalf("Save() error: %v", err)
-		}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err = storage.Save(tc.tasks)
+			if err != nil {
+				t.Fatalf("Save() error: %v", err)
+			}
 
-		got, err := storage.Load()
-		if err != nil {
-			t.Fatalf("Load() error: %v", err)
-		}
+			got, err := storage.Load()
+			if err != nil {
+				t.Fatalf("Load() error: %v", err)
+			}
 
-		if !reflect.DeepEqual(got, expected) {
-			t.Errorf("tasks = %+v, want = %+v", got, expected)
-		}
-	})
+			if !reflect.DeepEqual(got, tc.tasks) {
+				t.Errorf("tasks = %+v, want = %+v", got, tc.tasks)
+			}
+		})
+	}
 }
