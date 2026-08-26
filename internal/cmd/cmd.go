42266c5e45976c9c6c9ff191410601e728ce6dbf
IvanDetkov <ivandetkov.job@gmail.com>
2026-08-26 19:54:45 +0300

    add cmd package; implement entry point

---
+++ internal/cmd/cmd.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/TechOutsiders/TaskCLI/internal/repository"
+	"github.com/TechOutsiders/TaskCLI/internal/service"
+)
+
+const storagePath = "./data/tasks.json"
+
+func Run(args []string) error {
+	repository, err := repository.NewTaskStorage(storagePath)
+	if err != nil {
+		return fmt.Errorf("creating repository: %w", err)
+	}
+
+	service := service.NewService(repository)
+
+	if len(args) < 2 {
+		return fmt.Errorf("command is required")
+	}
+
+	switch args[1] {
+	case "add":
+		//TODO: implement add comand
+	case "list":
+		//TODO: implement list comand
+	case "show":
+		// TODO: implement show command
+	case "delete":
+		// TODO: implement delete command
+	case "edit":
+		// TODO: implement edit command
+	case "status":
+		// TODO: implement status command
+	case "priority":
+		// TODO: implement priority command
+	default:
+		return fmt.Errorf("unknown command: %s", args[1])
+	}
+
+	_ = service
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/TechOutsiders/TaskCLI/internal/cmd"
+)
+
+func main() {
+	err := cmd.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
